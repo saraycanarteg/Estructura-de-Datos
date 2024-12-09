9afe753390e319c927b4c1bb0ae40aaee9dd0e97
(ayuquina) <dmayuquina@espe.edu.ec>
2024-12-08 22:51:00 -0500

    menu ampliadp

--- Trabajos grupales/Proyecto/main.cpp
+++ Trabajos grupales/Proyecto/main.cpp
@@ -1,10 +1,12 @@
-#include "Menu.cpp"
+#include <stdlib.h>
 #include <iostream>
+#include "Menu.cpp"
 #include "Libro.h"
 #include "Libro.cpp"
 #include "Autor.cpp" 
 #include "Fecha.cpp"
 #include "ListaLibros.cpp"
+#include "Validation.cpp"
 /*#include "Libro.h"
 #include "Autor.h" 
 #include "Fecha.h"
@@ -13,8 +15,12 @@
 using namespace std;
 
 int main(){
-    Menu main_menu;
+
+    Validation<string> validacion_string;
+
     ListaLibros listaLibros;
+    
+    Menu main_menu;
     main_menu.set_menu("1. Ingresar libro");
     main_menu.set_menu("2. Buscar libro");
     main_menu.set_menu("3. Mostrar libreria");
@@ -44,68 +50,132 @@ int main(){
     Libro* libroEncontrado;
 
     int op;
+    int sub_menu_op;
 
     do{  
         op = main_menu.coursor("REGISTRO Y CONSULTA DE LIBROS");
         cout<<op<<endl;
 
-        switch (op)
-        {
+        switch (op){
         case 1:{
-            cout << "Ingrese el titulo del libro: ";
-            cin.ignore();
-            getline(cin, titulo);
-            cout << "Ingrese el autor del libro: ";
             string nombreAutor, apellidoAutor;
-            cin >> nombreAutor >> apellidoAutor ;
-            Autor autorLibro(nombreAutor, apellidoAutor);
-            cout << "Ingrese la fecha de publicacion (dd mm aaaa): ";
-            int dia, mes, anio;
-            cin >> dia >> mes >> anio;
-            Fecha fechaPublicacion(dia, mes, anio);
-            cout << "Ingrese el ISBN del libro: ";
-            cin >> isbn;
-            Libro nuevoLibro(titulo, autorLibro, fechaPublicacion, isbn);
-            listaLibros.registrarLibro(nuevoLibro);
-            cout << "Libro registrado exitosamente." << endl;
-            break;
-            }
-        case 2:{
-            op = search_menu.coursor("BUSQUEDA DE LIBROS");
-            cout << "Ingrese el título del libro a buscar: ";
-            cin.ignore();
-            getline(cin, titulo);
-            libroEncontrado = listaLibros.buscarLibroPorTitulo(titulo);
-            if (libroEncontrado) {
-                cout << "Libro encontrado:" << endl;
-                cout << "Titulo: " << libroEncontrado->getTitulo() << endl;
-                cout << "Autor: " << libroEncontrado->getAutor().getNombreCompleto() << endl;
-                cout << "Fecha de publicacion: " << libroEncontrado->getFechaPublicacion().getFechaComoString() << endl;
-                cout << "ISBN: " << libroEncontrado->getIsbn() << endl;
-            } else {
-                cout << "No se encontró el libro." << endl;
-            }
-            break;}
-        case 3:{
-            op = show_menu.coursor("IMPRESION DE DATOS");
 
-            listaLibros.imprimirLibros();
-            break;}
+            sub_menu_op = book_entry_menu.coursor("REGISTRO DE LIBROS");
+            system("cls");
+
+            if(sub_menu_op==2){
+                break;
+            }else{
+                cout<<"INGRESO DE LIBRO\n\n";
+                cout << "Ingrese el titulo del libro: ";    
+                titulo = validacion_string.enter_a_char_or_string_with_may(20);
+                cout << "\nIngrese los nombres del autor: ";
+                nombreAutor = validacion_string.enter_a_char_or_string_with_may(20);
+                cout << "\nIngrese los apellidos del autor: ";
+                apellidoAutor = validacion_string.enter_a_char_or_string_with_may(20);
+                Autor autorLibro(nombreAutor, apellidoAutor);
+
+                cout << "\nIngrese la fecha de publicacion (dd mm aaaa): ";
+                int dia, mes, anio;         //Ingrese validaciones de fecha
+                cin >> dia >> mes >> anio;
+                fflush(stdin);
+                Fecha fechaPublicacion(dia, mes, anio);
+
+                cout << "\nIngrese el ISBN del libro: "; //Ingrese validaciones isbn
+                cin >> isbn;
+                fflush(stdin);
+                Libro nuevoLibro(titulo, autorLibro, fechaPublicacion, isbn);
+                listaLibros.registrarLibro(nuevoLibro);
+                cout << "\nLibro registrado exitosamente." << endl;
+                system("pause");
+            }
+            break;
+        }
+        case 2:{
+            sub_menu_op = search_menu.coursor("BUSQUEDA DE LIBROS");
+            system("cls");
+            switch (sub_menu_op){
+            case 1:{
+                cout<<"BUSQUEDA POR LIBRO\n\n";
+                cout<<"Ingrese el titulo de libro: ";
+                titulo = validacion_string.enter_a_char_or_string_with_may(20);
+                libroEncontrado = listaLibros.buscarLibroPorTitulo(titulo);
+                if (libroEncontrado) {
+                    cout << "Libro encontrado:" << endl;
+                    cout << "Titulo: " << libroEncontrado->getTitulo() << endl;
+                    cout << "Autor: " << libroEncontrado->getAutor().getNombreCompleto() << endl;
+                    cout << "Fecha de publicacion: " << libroEncontrado->getFechaPublicacion().getFechaComoString() << endl;
+                    cout << "ISBN: " << libroEncontrado->getIsbn() << endl;
+                    system("pause");
+                } else {
+                    cout << "\n\nNo se encontro el libro.\n" << endl;
+                    system("pause");
+                }
+                break;
+            }
+            case 2:
+                cout<<"\nBUSQUEDA POR ISBN\n\n";
+                system("pause");
+                break;
+            case 3:
+                break;
+            }
+            break;
+        }
+        case 3:{
+            sub_menu_op = show_menu.coursor("IMPRESION DE DATOS");
+            system("cls");
+            switch (sub_menu_op){
+            case 1:
+                cout<<"LIBROS DEL AUTOR: "<<endl;
+                //TODO
+                system("pause");
+                break;
+            case 2:
+                listaLibros.imprimirLibros();
+                system("pause");
+                break;
+            case 3:
+                /* code */
+                break;
+            }
+
+            break;
+        }
         case 4:{
             
-            op = elimination_menu.coursor("ESCOJA UN LIBRO PARA ELIMINAR");
-            cout << "Ingrese el titulo del libro a eliminar: ";
-            cin.ignore();
-            getline(cin, titulo);
-            if (listaLibros.eliminarLibroPorTitulo(titulo)) {
-                cout << "Libro eliminado exitosamente." << endl;
-            } else {
-                cout << "No se encontro un libro con ese titulo." << endl;
+            sub_menu_op = elimination_menu.coursor("ESCOJA UN LIBRO PARA ELIMINAR");
+            system("cls");
+            switch (sub_menu_op){
+            case 1:{
+                cout<<"ELIMINACION POR TITULO"<<endl<<endl;
+                cout << "Ingrese el titulo del libro a eliminar: ";
+                titulo = validacion_string.enter_a_char_or_string_with_may(20);
+                cout<<endl<<endl;
+
+                if (listaLibros.eliminarLibroPorTitulo(titulo)) {
+                    cout << "Libro eliminado exitosamente." << endl;
+                } else {
+                    cout << "No se encontro un libro con ese titulo." << endl;
+                }
+                system("pause");
+                break;
             }
-            break;}
+            case 2:{
+                cout<<"ELIMINACION POR ISBN"<<endl;
+                /* code */
+                system("pause");
+                break;
+            }
+            case 3:
+                break;
+            }
+            break;
         }
-
+            default:
+                cout<<"GRACIAS POR USARNOS\n";
+                cout<<op;
+            break;
+        }
     }while(op!=5);
-    
-
 }
